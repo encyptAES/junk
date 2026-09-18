a9826f638238402322682512e6152175c5c85019
ency <53283017+encyptAES@users.noreply.github.com>
2026-09-18 22:30:34 +0700

    Update API URLs in PayloadRepository.kt

--- app/src/main/java/dev/busung/s25uroot/PayloadRepository.kt
+++ app/src/main/java/dev/busung/s25uroot/PayloadRepository.kt
@@ -140,10 +140,10 @@ class PayloadRepository(private val context: Context) {
 
     companion object {
         private const val COMMIT_API_URL =
-            "https://api.github.com/repos/zainarbani/Root-My-Galaxy-Payloads/git/ref/heads/a54x-test"
+            "https://api.github.com/repos/encyptAES/Root-My-Galaxy-Payloads/git/ref/heads/a54x"
         private const val RAW_REPOSITORY =
-            "https://raw.githubusercontent.com/zainarbani/Root-My-Galaxy-Payloads"
-        private const val MUTABLE_RAW_PREFIX = "$RAW_REPOSITORY/a54x-test/"
+            "https://raw.githubusercontent.com/encyptAES/Root-My-Galaxy-Payloads"
+        private const val MUTABLE_RAW_PREFIX = "$RAW_REPOSITORY/a54x/"
         private const val MAX_COMMIT_RESPONSE_BYTES = 16 * 1024
         private const val MAX_MANIFEST_BYTES = 256 * 1024
     }
